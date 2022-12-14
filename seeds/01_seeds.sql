2cb7256e7d8f651805698ee04349ffdbb73ef8a3
Steve Willsmore <scwillsmore@gmail.com>
2022-12-14 16:56:44 -0500

    Add seed data.

---
+++ seeds/01_seeds.sql
@@ -0,0 +1,30 @@
+INSERT INTO users (id, name, email, password)
+VALUES 
+(DEFAULT, 'Eva Stanley', 'sebastianguerra@ymail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
+
+(DEFAULT, 'Louisa Meyer', 'jacksonrose@hotmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
+
+(DEFAULT, 'Dominic Parks', 'victoriablackwell@outlook.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
+
+
+INSERT INTO properties (id, owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, parking_spaces, number_of_bathrooms, number_of_bedrooms, country, street, city, province, post_code, active)
+VALUES
+(DEFAULT, 1, 'Speed lamp', 'description', 'https://images.pexels.com/photos/2086676/pexels-photo-2086676.jpeg?auto=compress&cs=tinysrgb&h=350', 'https://images.pexels.com/photos/2086676/pexels-photo-2086676.jpeg', 930.61, 6, 4, 8, 'Canada', '536 Namsub Highway', 'Sotboske', 'Quebec', '28142', true),
+
+(DEFAULT, 1, 'Blank corner', 'description', 'https://images.pexels.com/photos/2121121/pexels-photo-2121121.jpeg?auto=compress&cs=tinysrgb&h=350', 'https://images.pexels.com/photos/2121121/pexels-photo-2121121.jpeg', 852.34, 6, 6, 7, 'Canada', '651 Nami Road', 'Bohbatev', 'Alberta', '83680', true),
+
+(DEFAULT, 2, 'Habit mix', 'description', 'https://images.pexels.com/photos/2080018/pexels-photo-2080018.jpeg?auto=compress&cs=tinysrgb&h=350', 'https://images.pexels.com/photos/2080018/pexels-photo-2080018.jpeg', 460.58, 0, 5, 6, 'Canada', '1650 Hejto Center', 'Genwezuj', 'Newfoundland And Labrador', '44583', true);
+
+
+INSERT INTO reservations (id, start_date, end_date, property_id, guest_id)
+VALUES
+(DEFAULT, '2018-09-11', '2018-09-26', 2, 3),
+(DEFAULT, '2019-01-04', '2019-02-01', 2, 2),
+(DEFAULT, '2023-10-01', '2023-10-14', 1, 3);
+
+
+INSERT INTO property_reviews (id, guest_id, property_id, reservation_id, rating, message)
+VALUES
+(DEFAULT, 3, 2, 1, 3, 'messages'),
+(DEFAULT, 2, 2, 2, 4, 'messages'),
+(DEFAULT, 3, 1, 3, 4, 'messages');
